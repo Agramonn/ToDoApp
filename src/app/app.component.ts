9a68d72a4b92cf41a98309e3f084b0e81cf72230
Agramon <pedrogutierrez08@outlook.com>
2024-01-22 18:06:57 -0700

    Adding the function addItem for adding a new todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'To do application';
+  taskTitle = 'Read cup of coffe';
+  isComplete = false;
+
+  filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false },
+  ];
+
+  completeTask(){
+    this.isComplete = true;
+  }
+
+  updateTitle(newTitle: string){
+    this.taskTitle = newTitle;
+  }
+  
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
 }
